8c5550b825f31243def777f4989aedf6c034fdb5
vchaplinski <107477833+vchaplinski@users.noreply.github.com>
2022-06-16 18:50:23 +0300

    Implement simple working up2date client in wrapperdll

--- wrapperdll/ddiclient.cpp
+++ wrapperdll/ddiclient.cpp
@@ -1,22 +1,49 @@
+#include <exception>
+#include <thread>
 #include "pch.h"
 #include "ddiclient.h"
-#include "ddi.hpp"
+#include "..\ddi\include\ddi.hpp"
+#include "..\dps\include\ritms_dps.hpp"
+#include "CallbackDispatcher.hpp"
+#include "DPSInfoReloadHandler.hpp"
+
+using namespace ritms::dps;
+using namespace ddi;
 
 namespace HkbClient {
 
-	int __stdcall TestFunction1(int a1, int a2)
-	{
+	int __stdcall TestFunction1(int a1, int a2)	{
 		return a1 + a2;
 	}
 
-    void _stdcall BuildAndRun(void)
-    {
-        auto builder = ddi::DDIClientBuilder::newInstance();
-        builder->setHawkbitEndpoint("hawkbitEndpoint", "controllerId");
-        builder->setGatewayToken("gatewayToken");
-        //builder->setEventHandler(std::shared_ptr<EventHandler>(new Handler()));
-        builder->notVerifyServerCertificate();
-        auto client = builder->build();
-        client->run();
+	int __stdcall TestFunction2(const char* s)	{
+		return std::string(s).length();
+	}
+
+    void __stdcall RunClient(const char* clientCertificatePath, const char* provisioningEndpoint, const char* xApigToken) {
+        std::ifstream t((std::string(clientCertificatePath)));
+        if (!t.is_open()) {
+            std::cout << "File " << clientCertificatePath << " not exists" << std::endl;
+            throw std::runtime_error(std::string("fail: cannot open file :").append(clientCertificatePath));
+        }
+        std::string crt((std::istreambuf_iterator<char>(t)),
+                        std::istreambuf_iterator<char>());
+
+        auto dpsBuilder = CloudProvisioningClientBuilder::newInstance();
+        auto dpsClient = dpsBuilder->setEndpoint(provisioningEndpoint)
+                ->setAuthCrt(crt)
+                ->addHeader("X-Apig-AppCode", std::string(xApigToken))
+                ->build();
+
+        auto authErrorHandler = std::shared_ptr<AuthErrorHandler>(new DPSInfoReloadHandler(std::move(dpsClient)));
+
+
+        auto builder = DDIClientBuilder::newInstance();
+        builder->setAuthErrorHandler(authErrorHandler)
+            ->setEventHandler(std::shared_ptr<EventHandler>(new CallbackDispatcher()))
+            ->build()
+            ->run();
+
     }
+
 }
--- wrapperdll/ddiclient.h
+++ wrapperdll/ddiclient.h
@@ -1,11 +1,12 @@
 #pragma once
-#include "ddi.hpp"
+#include "..\ddi\include\ddi.hpp"
 #include "dllexport.h"
 
 namespace HkbClient {
-	extern "C"
-	{
+	extern "C" {
 		WDLL_EXPORT int __stdcall TestFunction1(int, int);
-		WDLL_EXPORT void _stdcall BuildAndRun(void);
+		WDLL_EXPORT int __stdcall TestFunction2(const char* s);
+
+		WDLL_EXPORT void __stdcall RunClient(const char* clientCertificatePath, const char* provisioningEndpoint, const char* xApigToken);
 	}
 }
