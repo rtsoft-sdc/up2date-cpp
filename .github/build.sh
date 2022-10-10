fe63491a56757d6359847bd269f0bab15488a006
Aleksandrov Aleksander <aaleksandrov@dev.rtsoft.ru>
2022-10-10 13:28:00 +0300

    ci pipeline: fix build bug (example build flag)

--- .github/build.sh
+++ .github/build.sh
@@ -2,5 +2,5 @@
 
 BUILD_DIR=/var/up2date-cpp/
 
-cd "$BUILD_DIR" && rm -rf build && mkdir build && cd build && cmake ..
+cd "$BUILD_DIR" && rm -rf build && mkdir build && cd build && cmake -DBUILD_EXAMPLES=ON ..
 make && cd "$BUILD_DIR" && chmod -R 777 build
